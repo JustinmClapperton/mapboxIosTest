b6412e554a4bc83a90ec143e0a4dc538b63965e4
Justin Clapperton <justin@routific.com>
2021-04-12 09:01:06 -0700

    Initial Commit

---
+++ MapboxIosTest/MapboxIosTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapboxIosTestApp.swift
+//  MapboxIosTest
+//
+//  Created by Justin Clapperton on 2021-04-12.
+//
+
+import SwiftUI
+
+@main
+struct MapboxIosTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
